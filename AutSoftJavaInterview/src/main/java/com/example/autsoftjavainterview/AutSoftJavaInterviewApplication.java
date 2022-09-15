4be1eddfb6ee04bf8176eac72b0df12a031dad3d
Juhasz <juhaszadam98@gmail.com>
2022-09-15 11:05:58 +0200

    Spring initialized

---
+++ AutSoftJavaInterview/src/main/java/com/example/autsoftjavainterview/AutSoftJavaInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.autsoftjavainterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutSoftJavaInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutSoftJavaInterviewApplication.class, args);
+	}
+
+}
---
+++ AutSoftJavaInterview/src/test/java/com/example/autsoftjavainterview/AutSoftJavaInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autsoftjavainterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutSoftJavaInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
